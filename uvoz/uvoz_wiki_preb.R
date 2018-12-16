393dc9be21fa6512f11e520ecc3c8f7f423b8239
nejclu <nejclu@users.noreply.github.com>
2018-12-16 14:49:07 +0100

    Preimenovani stolpci (v ang), združena tabela o številu prebivalcev in prirastu

--- uvoz/uvoz_wiki_preb.R
+++ uvoz/uvoz_wiki_preb.R
@@ -7,22 +7,25 @@ library(dplyr)
 link <- "https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
 stran <- html_session(link) %>% read_html()
 tabela_preb <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable plainrowheaders']") %>%
-  .[[1]] %>% html_table() %>% transmute(Država=`Country or area` %>% strapplyc("^([^[]*)") %>% unlist(),
+  .[[1]] %>% html_table() %>% transmute(Country=`Country or area` %>% strapplyc("^([^[]*)") %>% unlist(),
                                         prebivalstvo=`Population(1 July 2017)[3]` %>%
                                           parse_number(locale=locale(grouping_mark=",")))
 
 
 #Preimenovanje drugega stolpca
-names(tabela_preb)[names(tabela_preb)=="prebivalstvo"] <- "Število prebivalcev (2016)"
+names(tabela_preb)[names(tabela_preb)=="prebivalstvo"] <- "Population-2016"
 
 #Odstrani prvo vrstico (podatek za svet)
 tabela_preb <- tabela_preb[-1,]
 
 #Tabela z naravnim prirastom
 tabela_prir <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable plainrowheaders']") %>%
-  .[[1]] %>% html_table() %>% transmute(Država=`Country or area` %>% strapplyc("^([^[]*)") %>% unlist(),
-                                        Prirast=`Change` %>%
+  .[[1]] %>% html_table() %>% transmute(Country=`Country or area` %>% strapplyc("^([^[]*)") %>% unlist(),
+                                        Change=`Change` %>%
                                           parse_number(locale=locale(grouping_mark=",")))
 
 #Odstrani prvo vrstico (podatek za svet)
 tabela_prir <- tabela_prir[-1,]
+
+#Tabeli s podatki o prebivalstvu dodamo stolpec o prirastu
+tabela_preb$Change <- tabela_prir$Change
