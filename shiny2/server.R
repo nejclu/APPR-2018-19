14ed3de053e47da494139c46fde6e2bc467e777e
nejclu <nejclu@users.noreply.github.com>
2019-02-25 18:53:35 +0100

    Spremenjene oznake osi, dodane pike na geom_line

--- shiny2/server.R
+++ shiny2/server.R
@@ -3,9 +3,9 @@ library(shiny)
 function(input, output) {
   output$graf_sprem <- renderPlot({
     graf_s <- ggplot(tabela_skupna %>% filter(Country == input$drzava)) +
-      aes(x = Year, y = Happiness.Score) + geom_line() +
-      labs(title="blabal") + theme(plot.title = element_text(hjust = 0.5)) +
-      ylab("nekineki") + xlab("šeneki") + ylim(2.5, 8)  
+      aes(x = Year, y = Happiness.Score) + geom_line() + geom_point() +
+      labs(title="Sprememba stopnje sreče med leti 2015 in 2017") + theme(plot.title = element_text(hjust = 0.5)) +
+      ylab("Stopnja sreče") + xlab("Čas") + ylim(2.5, 8)  
     print(graf_s)
 
   })
