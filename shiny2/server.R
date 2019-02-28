015b86172dd591d20907742787ddde155d107a8f
nejclu <nejclu@users.noreply.github.com>
2019-02-28 12:02:51 +0100

    Popravljena x os pri grafu v shiny-ju, tabela je v slovenščini

--- shiny2/server.R
+++ shiny2/server.R
@@ -2,15 +2,13 @@ library(shiny)
 
 function(input, output) {
   output$graf_sprem <- renderPlot({
-    graf_s <- ggplot(tabela_skupna %>% filter(Country == input$drzava)) +
-      aes(x = Year, y = Happiness.Score) + geom_line() + geom_point() +
-      labs(title="Sprememba stopnje sreče med leti 2015 in 2017") + theme(plot.title = element_text(hjust = 0.5)) +
-      ylab("Stopnja sreče") + xlab("Čas") + ylim(2.5, 8)  
+    graf_s <- ggplot(tabela_skupna %>% filter(Country == input$drzava), aes(x = factor(Year), y = Happiness.Score)) +
+      geom_line(aes(group=Country)) + geom_point() + labs(title="Sprememba stopnje sreče med leti 2015 in 2017") + 
+      theme(plot.title = element_text(hjust = 0.5)) + ylab("Stopnja sreče") + xlab("Čas") + ylim(2.5, 8)  
     print(graf_s)
-    
+#Če pr geom_line nism dodal aes(group=Country), ni telo povezat točk (po tem ko sm dau x=factor(Year)    
     tabela_shiny_sub <- reactive({
-      #tabela_shiny <- data.frame(tabela_shiny)
-      a <- tabela_shiny[tabela_shiny$Country == as.character(input$drzava),]
+      a <- tabela_shiny[tabela_shiny$Država == as.character(input$drzava),]
       return(a)
     })
     
