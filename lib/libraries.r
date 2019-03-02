1e2fea42f4054796a0e7921fba54176ae806d058
nejclu <nejclu@users.noreply.github.com>
2019-03-02 14:39:10 +0100

    Dodane potrebne knjižnice v datoteko.

--- lib/libraries.r
+++ lib/libraries.r
@@ -3,6 +3,12 @@ library(rvest)
 library(gsubfn)
 library(reshape2)
 library(shiny)
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(corrplot)
+library(tidyr)
+library(data.table)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
