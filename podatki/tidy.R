0ac3ebec50e9dff4ec78621709542cda361bcf7f
nejclu <nejclu@users.noreply.github.com>
2019-03-07 15:01:32 +0100

    Shranjeni tabeli v tidy data

--- podatki/tidy.R
+++ podatki/tidy.R
@@ -1,4 +1,6 @@
-tabela1_tidy <- melt(tabela_skupna, id.vars = c("Country","Year"), measure.vars = names(tabela_skupna)[-2:-1], variable.name="Spremenljivka", value.name="Vrednost")
+#Za tidy data ne potrebujemo stolpcev Happiness.Rank in Happiness.Score, saj jih lahko naračunamo iz ostalih podatkov
+pomozna_tidy <- tabela_skupna[-3:-4]
+tabela1_tidy <- melt(pomozna_tidy, id.vars = c("Country","Year"), measure.vars = names(pomozna_tidy)[-2:-1], variable.name="Spremenljivka", value.name="Vrednost")
 names(tabela1_tidy)[names(tabela1_tidy)=="Country"] <- "Država"
 names(tabela1_tidy)[names(tabela1_tidy)=="Year"] <- "Leto"
 
