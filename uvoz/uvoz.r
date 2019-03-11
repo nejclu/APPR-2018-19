1426d3b262eb1999c74dfa04925aa412a6622750
nejclu <nejclu@users.noreply.github.com>
2019-03-11 17:44:30 +0100

    Popravki pri preimenovanju stolpcev tabel

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -12,7 +12,7 @@ tabela_preb <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable pl
                                           parse_number(locale=locale(grouping_mark=",")))
 
 #Preimenovanje drugega stolpca
-names(tabela_preb)[names(tabela_preb)=="Population"] <- "Population(2016)"
+names(tabela_preb)[2] <- "Population(2016)"
 
 #Odstrani prvo vrstico (podatek za svet), ker je ne potrebujemo
 tabela_preb <- tabela_preb[-1,]
@@ -30,7 +30,7 @@ tabela_prir <- tabela_prir[-1,]
 tabela_preb$Change <- tabela_prir$Change
 
 #Preimenovan tretji stolpec
-names(tabela_preb)[names(tabela_preb)=="Change"] <- "Change(2016/2017)"
+names(tabela_preb)[3] <- "Change(2016/2017)"
 
 
 ###2015###
@@ -50,12 +50,12 @@ tabela_2015 <- tabela_2015[,c(1,11,2,3,4,5,6,7,8,9,10)]
 tabela_2015$`Happiness Rank` <- 1:148
 
 #Preimenujemo stolpce v katerih so presledki
-names(tabela_2015)[names(tabela_2015)=="Happiness Rank"] <- "Happiness.Rank"
-names(tabela_2015)[names(tabela_2015)=="Happiness Score"] <- "Happiness.Score"
-names(tabela_2015)[names(tabela_2015)=="Economy (GDP per Capita)"] <- "Economy"
-names(tabela_2015)[names(tabela_2015)=="Health (Life Expectancy)"] <- "Life.Expectancy"
-names(tabela_2015)[names(tabela_2015)=="Trust (Government Corruption)"] <- "Trust"
-names(tabela_2015)[names(tabela_2015)=="Dystopia Residual"] <- "Dystopia.Residual"
+names(tabela_2015)[3] <- "Happiness.Rank"
+names(tabela_2015)[4] <- "Happiness.Score"
+names(tabela_2015)[5] <- "Economy"
+names(tabela_2015)[7] <- "Life.Expectancy"
+names(tabela_2015)[9] <- "Trust"
+names(tabela_2015)[11] <- "Dystopia.Residual"
 
 ###2016###
 tabela_2016 <- read_csv("podatki/2016_changed.csv", locale=locale(encoding="Windows-1250"))
@@ -69,12 +69,12 @@ tabela_2016 <- tabela_2016[,c(1,11,2,3,4,5,6,7,8,9,10)]
 tabela_2016$`Happiness Rank` <- 1:148
 
 #Preimenujemo stolpce v katerih so presledki
-names(tabela_2016)[names(tabela_2016)=="Happiness Rank"] <- "Happiness.Rank"
-names(tabela_2016)[names(tabela_2016)=="Happiness Score"] <- "Happiness.Score"
-names(tabela_2016)[names(tabela_2016)=="Economy (GDP per Capita)"] <- "Economy"
-names(tabela_2016)[names(tabela_2016)=="Health (Life Expectancy)"] <- "Life.Expectancy"
-names(tabela_2016)[names(tabela_2016)=="Trust (Government Corruption)"] <- "Trust"
-names(tabela_2016)[names(tabela_2016)=="Dystopia Residual"] <- "Dystopia.Residual"
+names(tabela_2016)[3] <- "Happiness.Rank"
+names(tabela_2016)[4] <- "Happiness.Score"
+names(tabela_2016)[5] <- "Economy"
+names(tabela_2016)[7] <- "Life.Expectancy"
+names(tabela_2016)[9] <- "Trust"
+names(tabela_2016)[11] <- "Dystopia.Residual"
 
 ###2017###
 tabela_2017 <- read_csv("podatki/2017_changed.csv", locale=locale(encoding="Windows-1250"))
@@ -87,12 +87,12 @@ tabela_2017 <- tabela_2017[,c(1,11,2,3,4,5,6,7,8,9,10)]
 tabela_2017$`Happiness Rank` <- 1:148
 
 #Preimenujemo stolpce v katerih so presledki
-names(tabela_2017)[names(tabela_2017)=="Happiness Rank"] <- "Happiness.Rank"
-names(tabela_2017)[names(tabela_2017)=="Happiness Score"] <- "Happiness.Score"
-names(tabela_2017)[names(tabela_2017)=="Economy (GDP per Capita)"] <- "Economy"
-names(tabela_2017)[names(tabela_2017)=="Health (Life Expectancy)"] <- "Life.Expectancy"
-names(tabela_2017)[names(tabela_2017)=="Trust (Government Corruption)"] <- "Trust"
-names(tabela_2017)[names(tabela_2017)=="Dystopia Residual"] <- "Dystopia.Residual"
+names(tabela_2017)[3] <- "Happiness.Rank"
+names(tabela_2017)[4] <- "Happiness.Score"
+names(tabela_2017)[5] <- "Economy"
+names(tabela_2017)[7] <- "Life.Expectancy"
+names(tabela_2017)[10] <- "Trust"
+names(tabela_2017)[11] <- "Dystopia.Residual"
 
 ###Združi vse tri tabele##
 tabela_skupna <- rbind(tabela_2015, tabela_2016, tabela_2017)
@@ -139,8 +139,8 @@ tabela_2017 <- tabela_2017[,c(1,12,2,3,4,5,6,7,8,9,10,11)]
 ###
 #Tabele v tidydata
 pomozna_tidy <- tabela_skupna[-3:-4]
-names(pomozna_tidy)[names(pomozna_tidy)=="Country"] <- "Država"
-names(pomozna_tidy)[names(pomozna_tidy)=="Year"] <- "Leto"
+names(pomozna_tidy)[1] <- "Država"
+names(pomozna_tidy)[2] <- "Leto"
 
 tidy_economy <- pomozna_tidy[,c(1,2,3)]
 tidy_family <- pomozna_tidy[,c(1,2,4)]
@@ -150,21 +150,21 @@ tidy_trust <- pomozna_tidy[,c(1,2,7)]
 tidy_generosity <- pomozna_tidy[,c(1,2,8)]
 tidy_residual <- pomozna_tidy[,c(1,2,9)]
 
-names(tidy_economy)[names(tidy_economy)=="Economy"] <- "BDP"
-names(tidy_family)[names(tidy_family)=="Family"] <- "Družina"
-names(tidy_life.expectancy)[names(tidy_life.expectancy)=="Life.Expectancy"] <- "Pričakovana.življenjska.doba"
-names(tidy_freedom)[names(tidy_freedom)=="Freedom"] <- "Svoboda"
-names(tidy_trust)[names(tidy_trust)=="Trust"] <- "(Odsotnost).korupcije"
-names(tidy_generosity)[names(tidy_generosity)=="Generosity"] <- "Radodarnost"
-names(tidy_residual)[names(tidy_residual)=="Dystopia.Residual"] <- "Dystopia.Preostanek"
+names(tidy_economy)[3] <- "BDP"
+names(tidy_family)[3] <- "Družina"
+names(tidy_life.expectancy)[3] <- "Pričakovana.življenjska.doba"
+names(tidy_freedom)[3] <- "Svoboda"
+names(tidy_trust)[3] <- "(Odsotnost).korupcije"
+names(tidy_generosity)[3] <- "Radodarnost"
+names(tidy_residual)[3] <- "Dystopia.Preostanek"
 
 tidy_preb <- tabela_preb[-3]
 tidy_prir <- tabela_preb[-2]
 
-names(tidy_preb)[names(tidy_preb)=="Country"] <- "Država"
-names(tidy_preb)[names(tidy_preb)=="Population(2016)"] <- "Število.prebivalcev(2016)"
-names(tidy_prir)[names(tidy_prir)=="Country"] <- "Država"
-names(tidy_prir)[names(tidy_prir)=="Change(2016/2017)"] <- "Prirast(2016/2017)"
+names(tidy_preb)[1] <- "Država"
+names(tidy_preb)[2] <- "Število.prebivalcev(2016)"
+names(tidy_prir)[1] <- "Država"
+names(tidy_prir)[2] <- "Prirast(2016/2017)"
 
 create_empty_table <- function(num_rows, num_cols) {
   frame <- data.frame(matrix(NA, nrow = num_rows, ncol = num_cols))
@@ -175,12 +175,12 @@ prazna <- create_empty_table(233,2)
 tidy_prebivalstvo <- prazna
 tidy_prebivalstvo$X1 <- tidy_preb$Država
 tidy_prebivalstvo$X2 <- tidy_preb$`Število.prebivalcev(2016)`
-names(tidy_prebivalstvo)[names(tidy_prebivalstvo)=="X1"] <- "Država"
-names(tidy_prebivalstvo)[names(tidy_prebivalstvo)=="X2"] <- "Številoprebivalcev(2016)"
+names(tidy_prebivalstvo)[1] <- "Država"
+names(tidy_prebivalstvo)[2] <- "Število.prebivalcev(2016)"
 
 tidy_prirast <- prazna
 tidy_prirast$X1 <- tidy_prir$Država
 tidy_prirast$X2 <- tidy_prir$`Prirast(2016/2017)`
-names(tidy_prirast)[names(tidy_prirast)=="X1"] <- "Država"
-names(tidy_prirast)[names(tidy_prirast)=="X2"] <- "Prirast(2016/2017)"
+names(tidy_prirast)[1] <- "Država"
+names(tidy_prirast)[2] <- "Prirast(2016/2017)"
 ###
